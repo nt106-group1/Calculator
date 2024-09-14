01b25c13640659e3440727ec0bb5b58b68b7012e
congtuAnHuu <khoah0605@gmail.com>
2024-09-14 09:41:36 +0700

    MyCalculator

--- Calculator/Calculator/obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
+++ Calculator/Calculator/obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e0ef74db63bd33e2fa35b601fd7640973bd4b3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ec058a4ff870a6a8a8c1789a0659a9f44b026e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
