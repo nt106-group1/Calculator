3e0ef74db63bd33e2fa35b601fd7640973bd4b3a
congtuAnHuu <khoah0605@gmail.com>
2024-09-14 09:27:44 +0700

    MyCalculator

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            hienthi2 = new TextBox();
+            hienthi1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(14, 162);
+            button1.Name = "button1";
+            button1.Size = new Size(63, 44);
+            button1.TabIndex = 0;
+            button1.Text = "7";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(83, 162);
+            button2.Name = "button2";
+            button2.Size = new Size(63, 44);
+            button2.TabIndex = 1;
+            button2.Text = "8";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(152, 162);
+            button3.Name = "button3";
+            button3.Size = new Size(63, 44);
+            button3.TabIndex = 2;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(221, 212);
+            button4.Name = "button4";
+            button4.Size = new Size(63, 44);
+            button4.TabIndex = 3;
+            button4.Text = "*";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(221, 262);
+            button5.Name = "button5";
+            button5.Size = new Size(63, 44);
+            button5.TabIndex = 7;
+            button5.Text = "-";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(152, 212);
+            button6.Name = "button6";
+            button6.Size = new Size(63, 44);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(83, 212);
+            button7.Name = "button7";
+            button7.Size = new Size(63, 44);
+            button7.TabIndex = 5;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(14, 212);
+            button8.Name = "button8";
+            button8.Size = new Size(63, 44);
+            button8.TabIndex = 4;
+            button8.Text = "4";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(221, 312);
+            button9.Name = "button9";
+            button9.Size = new Size(63, 44);
+            button9.TabIndex = 11;
+            button9.Text = "+";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(152, 262);
+            button10.Name = "button10";
+            button10.Size = new Size(63, 44);
+            button10.TabIndex = 10;
+            button10.Text = "3";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(83, 262);
+            button11.Name = "button11";
+            button11.Size = new Size(63, 44);
+            button11.TabIndex = 9;
+            button11.Text = "2";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(14, 262);
+            button12.Name = "button12";
+            button12.Size = new Size(63, 44);
+            button12.TabIndex = 8;
+            button12.Text = "1";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(290, 262);
+            button13.Name = "button13";
+            button13.Size = new Size(63, 94);
+            button13.TabIndex = 15;
+            button13.Text = "=";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(152, 312);
+            button14.Name = "button14";
+            button14.Size = new Size(63, 44);
+            button14.TabIndex = 14;
+            button14.Text = ".";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(14, 312);
+            button15.Name = "button15";
+            button15.Size = new Size(132, 44);
+            button15.TabIndex = 13;
+            button15.Text = "0";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(221, 162);
+            button17.Name = "button17";
+            button17.Size = new Size(63, 44);
+            button17.TabIndex = 20;
+            button17.Text = "/";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += button17_Click;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(290, 212);
+            button18.Name = "button18";
+            button18.Size = new Size(63, 44);
+            button18.TabIndex = 19;
+            button18.Text = "C";
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += button18_Click;
+            // 
+            // button19
+            // 
+            button19.Location = new Point(290, 162);
+            button19.Name = "button19";
+            button19.Size = new Size(63, 44);
+            button19.TabIndex = 18;
+            button19.Text = "CE";
+            button19.UseVisualStyleBackColor = true;
+            button19.Click += button19_Click;
+            // 
+            // hienthi2
+            // 
+            hienthi2.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            hienthi2.Location = new Point(9, 29);
+            hienthi2.Multiline = true;
+            hienthi2.Name = "hienthi2";
+            hienthi2.Size = new Size(349, 42);
+            hienthi2.TabIndex = 21;
+            hienthi2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // hienthi1
+            // 
+            hienthi1.BackColor = SystemColors.Window;
+            hienthi1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            hienthi1.Location = new Point(9, 68);
+            hienthi1.Multiline = true;
+            hienthi1.Name = "hienthi1";
+            hienthi1.Size = new Size(349, 61);
+            hienthi1.TabIndex = 22;
+            hienthi1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(370, 370);
+            Controls.Add(hienthi1);
+            Controls.Add(hienthi2);
+            Controls.Add(button17);
+            Controls.Add(button18);
+            Controls.Add(button19);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "MyCalculator";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private TextBox hienthi2;
+        private TextBox hienthi1;
+    }
+}
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,181 @@
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private void SaveSetting()
+        {
+            Properties.Settings.Default.Location = this.Location;
+            Properties.Settings.Default.Height = this.Height;
+            Properties.Settings.Default.Width = this.Width;
+            Properties.Settings.Default.Save();
+        }
+        private void LoadSetting()
+        {
+            this.Location = Properties.Settings.Default.Location;
+            this.Height = Properties.Settings.Default.Height;
+            this.Width = Properties.Settings.Default.Width;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float data1, data2;
+        string pheptinh;
+
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "0";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            pheptinh = "cong";
+            data1 = float.Parse(hienthi1.Text);
+            hienthi1.Clear();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "1";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "2";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "3";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "4";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            hienthi1.Text = hienthi1.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + "9";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = hienthi1.Text + ".";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            hienthi1.Clear();
+            hienthi2.Clear();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (pheptinh == "cong")
+            {
+                data2 = data1 + float.Parse(hienthi1.Text);
+                hienthi2.Text = data1.ToString() + " + " + float.Parse(hienthi1.Text) + " = ";
+                hienthi1.Text = data2.ToString();
+            }
+            if (pheptinh == "tru")
+            {
+                data2 = data1 - float.Parse(hienthi1.Text);
+                hienthi2.Text = data1.ToString() + " - " + float.Parse(hienthi1.Text) + " = ";
+                hienthi1.Text = data2.ToString();
+            }
+            if (pheptinh == "nhan")
+            {
+                data2 = data1 * float.Parse(hienthi1.Text);
+                hienthi2.Text = data1.ToString() + " * " + float.Parse(hienthi1.Text) + " = ";
+                hienthi1.Text = data2.ToString();
+            }
+            if (pheptinh == "chia")
+            {
+                if (float.Parse(hienthi1.Text) == 0)
+                {
+                    hienthi2.Text = data1.ToString() + " / " + float.Parse(hienthi1.Text) + " = ";
+                    hienthi1.Text = "ERROR";
+                }
+                else
+                {
+                    data2 = data1 / float.Parse(hienthi1.Text);
+                    hienthi2.Text = data1.ToString() + " / " + float.Parse(hienthi1.Text) + " = ";
+                    hienthi1.Text = data2.ToString();
+                }
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pheptinh = "tru";
+            data1 = float.Parse(hienthi1.Text);
+            hienthi1.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pheptinh = "nhan";
+            data1 = float.Parse(hienthi1.Text);
+            hienthi1.Clear();
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+
+            pheptinh = "chia";
+            data1 = float.Parse(hienthi1.Text);
+            hienthi1.Clear();
+
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            hienthi1.Text = "0";
+
+            string f, s;
+
+            f = Convert.ToString(data1);
+            s = Convert.ToString(data2);
+
+            f = "";
+            s = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSetting();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SaveSetting();
+        }
+    }
+}
